3a46cf977c96e1553f9d789f77bbb1177846085c
Sohan KunkerKar <sakunker@ncsu.edu>
2019-04-27 14:22:02 -0400

    Upgrading kubebuilder version to v1.0.8

--- pkg/apis/clusterregistry/v1alpha1/types.go
+++ pkg/apis/clusterregistry/v1alpha1/types.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2017 The Kubernetes Authors.
+Copyright 2019 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@ limitations under the License.
 package v1alpha1
 
 import (
-	"k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
